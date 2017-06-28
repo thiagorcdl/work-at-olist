745b20c76c55ade2290c7b1c4b4160e4809d6dc5
Thiago Lima <thiagorcdl@gmail.com>
2017-06-28 01:53:46 -0300

    CHANGE database settings to PostgreSQL
    Using default configurations for temporary local development

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'workatolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'olist',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
