053fdcdd9e914b7ae7ac61f14f2a0da1a7c7f542
Thiago Lima <thiagorcdl@gmail.com>
2017-06-28 02:26:42 -0300

    ADD basic integration models structure
    Created the models for representing products, sellers, categories and channels along with the initial migration script.

--- work-at-olist/integration/models.py
+++ work-at-olist/integration/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
 
-# Work in progress
+class Channel(models.Model):
+    """The media where products are published"""
+    name = models.CharField(max_length=64)
+
+
+class Category(models.Model):
+    """A self-referencing model that represents a product's category or subcategory"""
+    channel = models.ForeignKey(Channel)
+    parent = models.ForeignKey('self', null=True, blank=True)
+    name = models.CharField(max_length=64)
+
+
+class Seller(models.Model):
+    """The users who are publishing products"""
+    name = models.CharField(max_length=64)
+
+
+class Product(models.Model):
+    """The object on sale"""
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=64)
