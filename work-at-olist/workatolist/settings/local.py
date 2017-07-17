7e5f7f8cab8c579ad7aee80aeea0206a5bf3b5d6
Thiago Lima <thiago@smartgreen.net>
2017-07-17 17:10:30 -0300

    CHANGE settings files location

---
+++ work-at-olist/workatolist/settings/local.py
@@ -0,0 +1,8 @@
+"""
+    Settings file for local development
+"""
+from .base import *
+
+INSTALLED_APPS.extends([
+    'django_extensions',
+])
--- work-at-olist/workatolist/settings/production.py
+++ work-at-olist/workatolist/settings/production.py
@@ -1,6 +1,10 @@
-
-from .settings import *
+"""
+    Settings file for production code
+"""
+from .base import *
 import dj_database_url
+
+DEBUG = False
 
 # Production code retrieves database information from environment variables
 db_from_env = dj_database_url.config(conn_max_age=300)
