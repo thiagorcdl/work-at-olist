61acde01360752d1c9e29366f24c4e58f17e4bbd
Thiago Lima <thiago@smartgreen.net>
2017-07-18 10:53:40 -0300

    ADD Heroku domain to allowed hosts

--- work-at-olist/settings/production.py
+++ work-at-olist/settings/production.py
@@ -9,3 +9,6 @@ DEBUG = False
 # Production code retrieves database information from environment variables
 db_from_env = dj_database_url.config(conn_max_age=300)
 DATABASES['default'].update(db_from_env)
+
+# Heroku domain clearance
+ALLOWED_HOSTS.extend([".herokuapp.com", ])
